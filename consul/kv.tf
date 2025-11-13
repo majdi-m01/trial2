9675c2210facfa88474e7cde0d283525de064708
Majdi <90557662+majdi-m01@users.noreply.github.com>
2025-11-13 09:23:16 +0100

    Delete consul directory

--- consul/kv.tf
+++
@@ -1,8 +0,0 @@
-resource "consul_keys" "echo_service" {
-
-  key {
-    path  = "echo/param1"
-    value = "param_from_consul"
-    delete = true
-  }
-}
--- consul/token.tf
+++
@@ -1,8 +0,0 @@
-resource "consul_acl_token" "nomad_agent" {
-  description = "Nomad Agent Token"
-  policies    = [consul_acl_policy.nomad_agent.name]
-  node_identities {
-    datacenter = "azure"
-    node_name = "nodeb"
-  }
-}
